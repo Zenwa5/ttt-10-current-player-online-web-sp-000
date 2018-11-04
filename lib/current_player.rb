172b417cb66f0c0fc86050c5c3505ce0c6c48356
Ezenwa Onukwue <ezenwa.onukwue@salesloft.com>
2018-11-04 18:02:03 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,13 +1,7 @@
 def turn_count(board)
   counter = 0
   board.each do |turn|
-    if turn == "X"
-    counter += 1
-  elsif
-    turn == "O"
-    counter += 1
-  else
     puts "#{counter}"
+    counter += 1
   end
-end
 end
